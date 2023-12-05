132f75ca6ddd99af73772d47cac30ae787f8df5f
Barbalob <a79221932005@gmail.com>
2023-12-05 14:51:26 +0500

    Роутинг

---
+++ src/router-list.js
@@ -0,0 +1,323 @@
+export const routerList = {
+    'main-page': `<div class="main-page">
+    <div class="main__header line-header">
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+    </label>
+    <nav class="header__nav">
+      <ul class="nav-list">
+        <li class="nav-list-item"><a href="#space-life-knowledge">Пространство для жизни и знания</a></li>
+        <li class="nav-list-item"><a href="#">Имперская ботаника</a></li>
+        <li class="nav-list-item"><a href="#">Человек и государственная власть</a></li>
+        <li class="nav-list-item"><a href="#">Человек и церковь</a></li>
+        <li class="nav-list-item"><a href="#">Человек и империя</a></li>
+        <li class="nav-list-item"><a href="#">Человек-семья-социум</a></li>
+        <li class="nav-list-item"><a href="#">Человек, добродетель и искоренение пороков</a></li>
+        <li class="nav-list-item"><a href="#">Образование: от традиций к новациям</a></li>
+      </ul>
+    </nav>
+  </div>
+
+    <h1 class="main__title">Название сайта</h1>
+    <div class="main__emblem">
+      <div class="emblem-wrapper">
+        <figure>
+          <img
+            class="emblem-img"
+            src="./assets/emblems/emblem1.png"
+            alt=""
+          />
+          <figcaption class="emblem-text">
+            Герб Российской Империи
+          </figcaption>
+        </figure>
+        <figure>
+          <img
+            class="emblem-img"
+            src="./assets/emblems/emblem2.png"
+            alt=""
+          />
+          <figcaption class="emblem-text">
+            Герб Британской Империи
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="main__description">
+      <div class="description-title">Добро пожаловать в музей</div>
+      <div class="description-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        autem earum, vitae dolorum facilis amet esse inventore! Corporis sed
+        cum numquam, nihil quibusdam magni voluptatum quidem quae molestiae
+        vitae porro! Praesentium nisi quo laboriosam expedita architecto,
+        qui exercitationem, magnam veritatis doloremque corporis autem
+        voluptas numquam. Eos fugit, iure reiciendis, deserunt maiores
+        repudiandae quia doloremque sequi voluptatibus a ratione nisi quod.
+        Quis omnis eligendi illum sunt, nihil recusandae porro vero modi
+        tempora nulla deserunt repellat doloremque ullam maxime quas! Illum,
+        tempora quis ullam corrupti laboriosam commodi odio mollitia
+        assumenda debitis asperiores.
+      </div>
+    </div>
+    
+    <div class="main__choice">
+      <button class="main__choice-button">Россия</button> 
+      <button class="main__choice-button">Британия</button>
+    </div>
+    <div class="main__sections"> 
+      <ul class="sections-list">
+        <li class="sections-list-item"><button class="sections-button"><a href='#space-life-knowledge'>Пространство для жизни и знания</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Имперская ботаника</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Человек и государственная власть</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Человек и церковь</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Человек и империя</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Человек-семья-социум </button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Человек, добродетель и искоренение пороков</button></a></li>
+        <li class="sections-list-item"><button class="sections-button"><a href='#'>Образование: от традиций к новациям</button></a></li>
+      </ul>
+    </div>
+  </div>`,
+  'space-life-knowledge' : `<div class="main-page">
+  <div class="main__header">
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+    </label>
+    <nav class="header__nav line-header">
+      <ul class="nav-list">
+        <li class="nav-list-item"><a href="#">Пространство для жизни и знания</a></li>
+        <li class="nav-list-item"><a href="#">Имперская ботаника</a></li>
+        <li class="nav-list-item"><a href="#">Человек и государственная власть</a></li>
+        <li class="nav-list-item"><a href="#">Человек и церковь</a></li>
+        <li class="nav-list-item"><a href="#">Человек и империя</a></li>
+        <li class="nav-list-item"><a href="#">Человек-семья-социум</a></li>
+        <li class="nav-list-item"><a href="#">Человек, добродетель и искоренение пороков</a></li>
+        <li class="nav-list-item"><a href="#">Образование: от традиций к новациям</a></li>
+      </ul>
+    </nav>
+  </div>
+  <h2 class="other__title">Пространство для жизни и знания</h2>    
+  <div class="choiceStat">            
+    <div class="emblem-wrapper-2">
+        <figure>
+          <img
+            class="emblem-img-2"
+            src="./assets/emblems/emblem1.png"
+            alt=""
+          />
+          <figcaption class="emblem-text-2">
+            Герб Российской Империи
+          </figcaption>
+        </figure>
+        <figure>
+          <img
+            class="emblem-img-2"
+            src="./assets/emblems/emblem2.png"
+            alt=""
+          />
+          <figcaption class="emblem-text-2">
+            Герб Британской Империи
+          </figcaption>
+        </figure>
+    </div>
+  </div>
+
+  <ul class="split-description-list">
+    <li class="split-description-list-item">
+      <p class="item-p">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, rem mollitia unde ea voluptas atque neque facere tenetur praesentium culpa quae in? Nostrum consequatur pariatur sequi quam. Nesciunt, voluptate voluptatum?            </p>
+      <a href="#section-screen-rus" class="item-a">Подробнее</a>
+    </li>
+    <li class="split-description-list-item">
+      <p class="item-p">
+        Описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание
+      </p>
+      <a href="#section-screen-en" class="item-a">Подробнее</a>
+    </li>
+  </ul>
+</div>`,
+  'section-screen-rus':`<div class="main-page">
+  <div class="main__header">
+    <input id="menu-toggle" type="checkbox" />
+    <label class="menu-button-container" for="menu-toggle">
+      <div class="menu-button"></div>
+    </label>
+    <nav class="header__nav">
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <a href="#">Пространство для жизни и знания</a>
+        </li>
+        <li class="nav-list-item"><a href="#">Имперская ботаника</a></li>
+        <li class="nav-list-item">
+          <a href="#">Человек и государственная власть</a>
+        </li>
+        <li class="nav-list-item"><a href="#">Человек и церковь</a></li>
+        <li class="nav-list-item"><a href="#">Человек и империя</a></li>
+        <li class="nav-list-item">
+          <a href="#">Человек-семья-социум</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="#">Человек, добродетель и искоренение пороков</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="#">Образование: от традиций к новациям</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <h1 class="main__title line-title">Название сайта</h1>
+
+  <h2 class="other__title">Пространство для жизни и знания</h2>
+
+
+  <div class="selected-description">
+    <a href='#section-screen-en'><img class="description-arrow" src="./assets/arrow.svg" alt=""></a>
+    <a disable> <img class="description-arrow description-arrow-left" src="./assets/arrow.svg" alt=""></a>
+    <div class="selected-description-item">
+      <figure class="item-emblem">
+        <img
+          class="item-emblem-img"
+          src="./assets/emblems/emblem1.png"
+          alt=""
+        />
+        <figcaption class="item-emblem-text">
+          Герб Российской Империи
+        </figcaption>
+      </figure>
+      <p class="item-description">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, corrupti assumenda? Est veritatis sint quasi incidunt quas eaque animi ab sed, necessitatibus dolorem eius reprehenderit, magnam labore, similique autem repellendus!
+        Beatae deserunt cum rerum illum necessitatibus, fugiat assumenda omnis eligendi reprehenderit placeat architecto consectetur doloremque repudiandae nisi laborum temporibus eos non? Quod voluptatem consequuntur eligendi hic, dolore voluptate enim ipsum.
+        Culpa, perferendis ex nesciunt, qui itaque beatae veniam temporibus architecto non, eaque enim quisquam delectus incidunt! Perspiciatis eius labore debitis molestias laborum dolor, iste voluptates illo eaque, expedita commodi voluptatum.
+      </p>
+      <h3 class="item-title">Список подразделов</h3>
+      <ul class="item-list-description">
+        <li class="list-description-item"><a class="list-description-item-link" href="#cards-rus">Сакральные пространства</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Переходная эпоха</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Территорификация</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Субъективность и воображение</a></li>
+      </ul>
+    </div>
+  </div>
+  
+</div>`,
+  'section-screen-en':`<div class="main-page">
+  <div class="main__header">
+    <input id="menu-toggle" type="checkbox" />
+    <label class="menu-button-container" for="menu-toggle">
+      <div class="menu-button"></div>
+    </label>
+    <nav class="header__nav">
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <a href="#">Пространство для жизни и знания</a>
+        </li>
+        <li class="nav-list-item"><a href="#">Имперская ботаника</a></li>
+        <li class="nav-list-item">
+          <a href="#">Человек и государственная власть</a>
+        </li>
+        <li class="nav-list-item"><a href="#">Человек и церковь</a></li>
+        <li class="nav-list-item"><a href="#">Человек и империя</a></li>
+        <li class="nav-list-item">
+          <a href="#">Человек-семья-социум</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="#">Человек, добродетель и искоренение пороков</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="#">Образование: от традиций к новациям</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <h1 class="main__title line-title">Название сайта</h1>
+
+  <h2 class="other__title">Пространство для жизни и знания</h2>
+
+
+  <div class="selected-description">
+    <a><img class="description-arrow" src="./assets/arrow.svg" alt=""></a>
+    <a href='#section-screen-rus'><img class="description-arrow description-arrow-left" src="./assets/arrow.svg" alt=""></a>
+    <div class="selected-description-item">
+      <figure class="item-emblem">
+        <img
+          class="item-emblem-img"
+          src="./assets/emblems/emblem2.png"
+          alt=""
+        />
+        <figcaption class="item-emblem-text">
+          Герб Российской Империи
+        </figcaption>
+      </figure>
+      <p class="item-description">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, corrupti assumenda? Est veritatis sint quasi incidunt quas eaque animi ab sed, necessitatibus dolorem eius reprehenderit, magnam labore, similique autem repellendus!
+        Beatae deserunt cum rerum illum necessitatibus, fugiat assumenda omnis eligendi reprehenderit placeat architecto consectetur doloremque repudiandae nisi laborum temporibus eos non? Quod voluptatem consequuntur eligendi hic, dolore voluptate enim ipsum.
+        Culpa, perferendis ex nesciunt, qui itaque beatae veniam temporibus architecto non, eaque enim quisquam delectus incidunt! Perspiciatis eius labore debitis molestias laborum dolor, iste voluptates illo eaque, expedita commodi voluptatum.
+      </p>
+      <h3 class="item-title">Список подразделов</h3>
+      <ul class="item-list-description">
+        <li class="list-description-item"><a class="list-description-item-link" href="#cards-en">Ранние травелоги и карты</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Открытие природы</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Субъективность и сентиментализм</a></li>
+        <li class="list-description-item"><a class="list-description-item-link" href="#">Картография и печать</a></li>
+      </ul>
+    </div>
+  </div>
+  
+</div>`,
+'cards-en': `<div class="wrapper-cards">
+<a href="#" ><img  class="cards-arrow" src="./assets/arrow.svg" alt=""></a>
+<a href="#"><img class="cards-arrow cards-arrow-left" src="./assets/arrow.svg" alt=""></a>
+  
+<div class="cards">
+  <h2 class="cards-title">Ранние травелоги и карты</h2>
+  <img
+        class="cards-img line-img"
+        src="./assets/test.png"
+        alt=""
+      />
+  <div class="line"></div>
+  <p class="cards-description">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, corrupti assumenda? Est veritatis sint quasi incidunt quas eaque animi ab sed, necessitatibus dolorem eius reprehenderit, magnam labore, similique autem repellendus!
+    Beatae deserunt cum rerum illum necessitatibus, fugiat assumenda omnis eligendi reprehenderit placeat architecto consectetur doloremque repudiandae nisi laborum temporibus eos non? Quod voluptatem consequuntur eligendi hic, dolore voluptate enim ipsum.
+    Culpa, perferendis ex nesciunt, qui itaque beatae veniam temporibus architecto non, eaque enim quisquam delectus incidunt! Perspiciatis eius labore debitis molestias laborum dolor, iste voluptates illo eaque, expedita commodi voluptatum.
+  </p>
+  <h3 class="cards-title-list">Список подразделов</h3>
+  <ul class="cards-list-description">
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Сакральные пространства</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Переходная эпоха</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Территорификация</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Субъективность и воображение</a></li>
+  </ul>
+</div>
+</div>`,
+'cards-rus':`<div class="wrapper-cards negative">
+<a href="#" ><img  class="cards-arrow" src="./assets/arrow.svg" alt=""></a>
+<a href="#"><img class="cards-arrow cards-arrow-left" src="./assets/arrow.svg" alt=""></a>
+  
+<div class="cards">
+  <h2 class="cards-title">Ранние травелоги и карты</h2>
+  <img
+        class="cards-img line-img"
+        src="./assets/test.png"
+        alt=""
+      />
+  <div class="line"></div>
+  <p class="cards-description">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, corrupti assumenda? Est veritatis sint quasi incidunt quas eaque animi ab sed, necessitatibus dolorem eius reprehenderit, magnam labore, similique autem repellendus!
+    Beatae deserunt cum rerum illum necessitatibus, fugiat assumenda omnis eligendi reprehenderit placeat architecto consectetur doloremque repudiandae nisi laborum temporibus eos non? Quod voluptatem consequuntur eligendi hic, dolore voluptate enim ipsum.
+    Culpa, perferendis ex nesciunt, qui itaque beatae veniam temporibus architecto non, eaque enim quisquam delectus incidunt! Perspiciatis eius labore debitis molestias laborum dolor, iste voluptates illo eaque, expedita commodi voluptatum.
+  </p>
+  <h3 class="cards-title-list">Список подразделов</h3>
+  <ul class="cards-list-description">
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Сакральные пространства</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Переходная эпоха</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Территорификация</a></li>
+    <li class="cards-list-description-item"><a class="cards-list-description-item-link" href="#">Субъективность и воображение</a></li>
+  </ul>
+</div>
+</div>`
+
+}
